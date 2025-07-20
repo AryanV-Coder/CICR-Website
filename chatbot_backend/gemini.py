e67727cd2fe84f080b4753b3db98646346595e15
AryanV-Coder <aryanv.india1@gmail.com>
2025-07-20 13:21:43 +0530

    1. Added markdown functionality to chatbot, 2. Fine tuned the model, 3. Added resizing functionality to the chatbot window

--- chatbot_backend/gemini.py
+++ chatbot_backend/gemini.py
@@ -6,7 +6,7 @@ import json
 load_dotenv()
 
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
-model = genai.GenerativeModel("gemini-2.0-flash-lite")
+model = genai.GenerativeModel("gemini-2.0-flash-lite",system_instruction="Always format your responses using proper Markdown syntax. Use **text** for bold, *text* for italics, and appropriate heading levels. Also reply in as minimimum words as possible. Only reply to CICR related queries. Remember you are made by CICR not GOOGLE. You shoud not take GOOGLE's name.")
 
 with open('cicrData.json','r') as file:
     cicrData = json.load(file)
